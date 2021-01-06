0c4b3ca4e27f2ec279befac6a599ffd32efe87a7
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2021-01-06 23:57:39 +0000

    +setup lowdb

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('data.json');
+const db = low(adapter);
+
 app.use(express.static('./static'));
 
 app.listen(port);
