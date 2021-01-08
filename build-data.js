8f7eccbea0e84a57640eeab804aa10e833035dd1
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2021-01-08 17:28:31 +0000

    ~fetch species before pokemon for more complete data

--- build-data.js
+++ build-data.js
@@ -3,9 +3,22 @@ const fs = require('fs').promises;
 
 fs.open('./data.json', 'wx').then( async file => {
     let data = {'mons': []}
+    let ids = [];
+    let idRegexp = /\b[0-9]{1,}/;
 
     for(let i = 0; i < 898; i++) {
-        let mon = await getMon(i + 1);
+        let species = await (await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${i+1}/`)).data;
+        for(let ii = 0; ii < species.varieties.length; ii++) {
+            console.log(species.varieties[ii].pokemon);
+            console.log(species.varieties[ii].pokemon.url.match(idRegexp)[0]);
+            let id = parseInt(species.varieties[ii].pokemon.url.match(idRegexp)[0]);
+            console.log(`id: ${id} | name: ${species.varieties[ii].pokemon.name}`);
+            ids.push();
+        }
+    }
+
+    for(let i = 0; i < ids.length; i++) {
+        let mon = await getMon(ids[i]);
         console.log(mon);
         if(mon) data.mons.push(mon)
     }
